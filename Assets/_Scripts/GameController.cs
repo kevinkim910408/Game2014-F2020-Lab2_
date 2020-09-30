3a4bdb335855f348bb8bb755c8d53959d1935f5c
kevinkim0408 <kevinkim0408@naver.com>
2020-09-30 18:17:38 +0900

    lab2 - responsive UI
    Unity

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    //public TMP_Text sceneLabel;
+    public TMP_Text LivesLabel;
+    public TMP_Text ScoreLabel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(Input.deviceOrientation)
+        {
+            case DeviceOrientation.LandscapeLeft:
+                LivesLabel.rectTransform.anchoredPosition = new Vector2(370f, -105f);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-349f, -105f);
+                break;
+
+            case DeviceOrientation.LandscapeRight:
+                LivesLabel.rectTransform.anchoredPosition = new Vector2(248f, -105f);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-490f, -105f);
+                break;
+
+            case DeviceOrientation.Portrait:
+                LivesLabel.rectTransform.anchoredPosition = new Vector2(248f, -185f);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-349f, -185f);
+                break;
+
+            case DeviceOrientation.Unknown:
+                break;
+        }
+    }
+
+  
+}
---
+++ Assets/_Scripts/TestButtonBehaviour.cs
@@ -0,0 +1,18 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class TestButtonBehaviour : MonoBehaviour
+{
+    //public TMP_Text sceneLabel;
+    public TMP_Text LivesLabel;
+    public TMP_Text ScoreLabel;
+    public void OnTestButtonPressed()
+    {
+        Debug.Log("TestButton Pressed");
+        LivesLabel.rectTransform.position = new Vector3(370f, -105f, 0.0f);
+        ScoreLabel.rectTransform.anchoredPosition = new Vector3(-349f, -105f, 0.0f);
+    }
+}
